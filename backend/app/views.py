860fa9c735feca7781c2dcff81cafcab1a41328f
Varun Jindal <vjindal0112@gmail.com>
2022-11-13 01:51:27 -0500

    finished implementing db table joins

--- backend/app/views.py
+++ backend/app/views.py
@@ -14,6 +14,10 @@ GOOGLE_API_KEY = "AIzaSyDYIhhUkPaSShIvqXprmycYyRyGfRxSrHs"
 GOOGLE_LANG_BASE_URL = "https://language.googleapis.com/v1/documents:analyzeEntities"
 
 
+# TEST_API_URL = "https://vsrtlryzcqlqltbxviqd.supabase.co"
+# TEST_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzcnRscnl6Y3FscWx0Ynh2aXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgzMTE1NjUsImV4cCI6MTk4Mzg4NzU2NX0.G5AhSqn_dH7I9bA12u-ZC5HQTOpHzfnMMeGAdvgoXHA"
+
+
 @csrf_exempt
 # @api_view(['GET', 'POST', 'DELETE', 'UPDATE'])
 def notes(request):
@@ -36,22 +40,6 @@ def notes(request):
     return JsonResponse(response)
 
 
-@csrf_exempt
-def notes_test(request):
-    body = {
-        "document": {
-            "content": "Ethan is a student at the University of Michigan. He is going to go hard for Halloween.",
-            "type": "PLAIN_TEXT"
-        },
-        "encodingType": "UTF8"
-    }
-    res = requests.post(GOOGLE_LANG_BASE_URL + "?key=" +
-                        GOOGLE_API_KEY, json=body)
-    print(json.dumps(res.json(), indent=2))
-
-    return JsonResponse(res.json())
-
-
 def generate_tags(text) -> list:
     body = {
         "document": {
@@ -68,9 +56,31 @@ def generate_tags(text) -> list:
     for entity in res.json()['entities']:
         if count > 5:
             break
-        tags.append(entity['name'])
+        tags.append({"name": entity['name'], "salience": entity['salience']})
         count += 1
+
+    print(tags)
     return tags
+
+
+def insert_tag(tag, note_id):
+    # tag: {"name": tag_name, "salience": salience}
+    # note_id: uuid
+    supabase = create_client(API_URL, API_KEY)
+    current_tag = supabase.table("tags").select(
+        "*").eq("name", tag['name']).execute()
+    print("CURRENT TAG")
+    print(current_tag)
+    if current_tag.data == []:
+        print("INSERTING TAG")
+        tag_id = str(uuid.uuid4())
+        supabase.table("tags").insert(
+            {"name": tag['name'], "tag_id": tag_id}).execute()
+        supabase.table("notes_tags").insert(
+            {"tag_id": tag_id, "note_id": note_id, "salience": tag['salience']}).execute()
+    else:
+        supabase.table("notes_tags").insert(
+            {"note_id": note_id, "tag_id": current_tag.data[0]["tag_id"], "salience": tag["salience"]}).execute()
 
 
 @csrf_exempt
@@ -78,27 +88,27 @@ def notes_post(request):
     supabase = create_client(API_URL, API_KEY)
     json_data = json.loads(request.body)
     note_id = str(uuid.uuid4())
-    currentTime = time.time()
     data = {
         'note_id': note_id,
         'content': json_data['content'],
         'deleted': False
     }
-    supabase.table('notes').insert(data).execute()  # inserting one record
-
     response = {}
-    response['note_id'] = note_id
     response['tags'] = generate_tags(json_data['content'])
-    # need to store tags in database
+    supabase.table('notes').insert(
+        data).execute()  # inserting one record
+    response['note_id'] = note_id
+    for tag in response['tags']:
+        insert_tag(tag, note_id)
+
     return JsonResponse(response)
 
 
 @csrf_exempt
 def notes_get(request):
     supabase = create_client(API_URL, API_KEY)
-    t = supabase.table('notes').select('*').execute()
-    # should include tags from the tags table
-    return JsonResponse({"notes": t.data})
+    all_notes = supabase.rpc('get_tags_for_notes', {}).execute()
+    return JsonResponse({"notes": all_notes.data})
 
 
 @csrf_exempt
@@ -113,16 +123,39 @@ def note_action(request, note_id):
 def notes_update(request, note_id):
     supabase = create_client(API_URL, API_KEY)
     json_data = json.loads(request.body)
-    new_tags = generate_tags(json_data['content'])
-    # should update tags table as well
     t = supabase.table('notes').update(
         {'content': json_data['content']}).eq('note_id', note_id).execute()
+    supabase.table("notes_tags").delete().eq("note_id", note_id).execute()
+    tags = generate_tags(json_data['content'])
+    for tag in tags:
+        insert_tag(tag, note_id)
     return JsonResponse({})
 
 
 @csrf_exempt
 def notes_delete(request, note_id):
     supabase = create_client(API_URL, API_KEY)
-    supabase.table('notes').delete().eq('note_id', note_id).execute()
-    # should also delete tags if no other notes have that tag
+    # supabase.table('notes').delete().eq('note_id', note_id).execute()
+    # supabase.table("notes_tags").delete().eq("note_id", note_id).execute()
+    supabase.table('notes').update(
+        {'deleted': True}).eq('note_id', note_id).execute()
+    supabase.table('notes_tags').update(
+        {'deleted': True}).eq('note_id', note_id).execute()
+    # TODO: delete tags if no other notes have that tag
     return JsonResponse({})
+
+
+@csrf_exempt
+def notes_test(request):
+    body = {
+        "document": {
+            "content": "Ethan is a student at the University of Michigan. He is going to go hard for Halloween.",
+            "type": "PLAIN_TEXT"
+        },
+        "encodingType": "UTF8"
+    }
+    res = requests.post(GOOGLE_LANG_BASE_URL + "?key=" +
+                        GOOGLE_API_KEY, json=body)
+    print(json.dumps(res.json(), indent=2))
+
+    return JsonResponse(res.json())
