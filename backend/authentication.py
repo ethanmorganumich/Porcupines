3bd4984e14524e1a140e6cca2a8910f76ce55982
mmisiak71 <mmisiak71@gmail.com>
2022-12-02 20:07:28 -0500

    fixed initial auth issues and added to gcp

--- backend/authentication.py
+++ backend/authentication.py
@@ -1,11 +1,11 @@
 from supabase import create_client
-import functions_framework
-from google.cloud import language_v1
-import json
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import functions_framework, os, re
 
-
-API_URL = 'https://lwheswmvrtoltfogtrvv.supabase.co'
-API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3aGVzd212cnRvbHRmb2d0cnZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcxNTUxMDgsImV4cCI6MTk4MjczMTEwOH0.2PL2EGizWXNXUh6T_wKuPM1KZrgJ0X41zsWGkR0lgJA'
+load_dotenv()
+API_URL = os.getenv("API_URL")
+API_KEY = os.getenv("API_KEY")
 
 @functions_framework.http
 def authentication_request(request):
@@ -17,21 +17,28 @@ def authentication_request(request):
 def user_post(request):
     supabase = create_client(API_URL, API_KEY)
     json_data = request.get_json()
-    res = supabase.auth.sign_up(email=json_data['email'], password=json_data['password'])
+    
+    res = supabase.auth.sign_up(
+        email=json_data['email'], 
+        password=json_data['password']
+    )
+    
     return ({
-        "user_id": res.id
+        "user_id": res.id,
     })
 
 def user_get(request):
     supabase = create_client(API_URL, API_KEY)
-    json_data = request.get_json() 
-    res = supabase.auth.sign_in(email=json_data['email'], password=json_data['password'])
+
+    json_data = re.split(';|=', urlparse(request.path).params)
+    
+    res = supabase.auth.sign_in(
+        email=json_data[1], 
+        password=json_data[3]
+    )
+    
     return ({
-        "token": res.access_token,
+        "access_token": res.access_token,
         "refresh_token": res.refresh_token,
         "user_id": res.user.id,
-        "email": res.user.email
     })
-
-# a1251f20-3a24-4fbc-b23e-8f38e2c2e64c
-
--- backend/utils.py
+++ backend/utils.py
@@ -1,7 +1,7 @@
+from urllib.parse import urlparse
 from supabase import create_client
 from dotenv import load_dotenv
-import jwt
-import os
+import jwt, os, re
 
 
 load_dotenv()
@@ -15,7 +15,17 @@ SECRET = os.getenv("SECRET")
 #   if(session is invalid) -> Exception explainging error
 #   else -> Dict containing access_token and refresh_token
 def check_valid_request(request):
-    json_data = request.get_json()
+
+    json_data = {}
+    if(request.method == 'GET'):
+        param_array = re.split(';|=', urlparse(request.path).params)
+        json_data = {
+            'access_token': param_array[1],
+            'refresh_token': param_array[3]
+        }
+    else:
+        json_data = request.get_json()
+
     check = check_JWT_token(json_data)
     ret = {
         'access_token': json_data['access_token'],
@@ -33,7 +43,6 @@ def check_valid_request(request):
     return ret
 
 
-
 # Parse JWT token to confirm that token session is valid and user is authenticated
 # Accepts a json representation of request data
 # Returns:
@@ -41,9 +50,6 @@ def check_valid_request(request):
 #   elif(token was refreshed) -> Dict containing new refresh_token and access_token
 #   else -> Exception explaining error
 def check_JWT_token(json_data):
-    
-    #jwt_token = jwt.decode(jwt=json_data['access_token'], key=SECRET ,algorithms=["HS256"], audience="authenticated")
-
     try: 
         jwt_token = jwt.decode(jwt=json_data['access_token'], key=SECRET ,algorithms=["HS256"], audience="authenticated")
     except:
@@ -71,9 +77,11 @@ def check_JWT_token(json_data):
             return Exception("could not refresh user session")
     return 1
 
+# Helper for merging dicts
 def Merge(dict1, dict2):
     if dict1: 
         res = {**dict1, **dict2}
     else:
         res = {**dict2}
     return res
+    
